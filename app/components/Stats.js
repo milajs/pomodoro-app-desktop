961647a7749138fe66110dbf6d05f13cf45110a0
mila.pro <lpromyslova@mail.ru>
2018-06-25 23:04:04 +0300

    fix series icon

--- app/components/Stats.js
+++ app/components/Stats.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import styles from './Stats.css'
 
+const tomato = require('../assets/tomato.png')
+
 const TOTAL = 12
 
 export default function Stats({ total, series }) {
-  const tomato = <img src="/tomato.png" alt="tomato" />
+  const icon = <img src={tomato} alt="tomato" />
 
   const totalPart = total / TOTAL
   const width = totalPart > 1 ? '300px' : `${totalPart * 300}px`
@@ -16,13 +18,13 @@ export default function Stats({ total, series }) {
     </p>,
 
     <div className={styles.seriesRow} key="series-row">
-      {series > 0 ? tomato : <div className={styles.series} />}
+      {series > 0 ? icon : <div className={styles.series} />}
 
-      {series > 1 ? tomato : <div className={styles.series} />}
+      {series > 1 ? icon : <div className={styles.series} />}
 
-      {series > 2 ? tomato : <div className={styles.series} />}
+      {series > 2 ? icon : <div className={styles.series} />}
 
-      {series > 3 ? tomato : <div className={styles.series} />}
+      {series > 3 ? icon : <div className={styles.series} />}
     </div>,
 
     <p className={styles.seriesTitle} key="total-title">
