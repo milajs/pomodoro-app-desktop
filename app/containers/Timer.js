1d7462f9a0c3a62535394eaa929a3e19a845e353
mila.pro <lpromyslova@mail.ru>
2018-07-15 16:55:59 +0300

    fix timer reset

--- app/containers/Timer.js
+++ app/containers/Timer.js
@@ -1,15 +1,15 @@
-import React, { PureComponent } from 'react'
-import { ipcRenderer } from 'electron'
+import React, { PureComponent } from 'react';
+import { ipcRenderer } from 'electron';
 
-import Timer from '../Components/Timer'
-import Stats from '../Components/Stats'
+import Timer from '../Components/Timer';
+import Stats from '../Components/Stats';
 
-import { formatTimeToString, getNewSeries } from '../utils/timer'
+import { formatTimeToString, getNewSeries } from '../utils/timer';
 
-import { WORK_TIME, RELAX_TIME } from '../constants'
+import { WORK_TIME, RELAX_TIME } from '../constants';
 
-const startSound = require('../assets/pomodoro-start.mp3')
-const endSound = require('../assets/pomodoro-end.mp3')
+const startSound = require('../assets/pomodoro-start.mp3');
+const endSound = require('../assets/pomodoro-end.mp3');
 
 const initialState = {
   series: 0,
@@ -17,27 +17,27 @@ const initialState = {
   active: false,
   time: WORK_TIME,
   stage: 'work'
-}
+};
 
 export default class MainContainer extends PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = initialState
+    this.state = initialState;
   }
 
   componentDidMount() {
     ipcRenderer.on('toggle-timer', () => {
-      this.toggleTimer()
-    })
+      this.toggleTimer();
+    });
 
     ipcRenderer.on('reset-timer', () => {
-      this.resetTimer()
-    })
+      this.resetTimer();
+    });
   }
 
   render() {
-    const { stage } = this.state
+    const { stage } = this.state;
 
     return (
       <div className={`container background-${stage}`}>
@@ -50,74 +50,74 @@ export default class MainContainer extends PureComponent {
             toggleTimer={this.toggleTimer}
           />
 
-          <Stats
-            total={this.state.total}
-            series={this.state.series}
-          />
+          <Stats total={this.state.total} series={this.state.series} />
 
           <audio id="audio-end" src={endSound} autostart="false" />
           <audio id="audio-start" src={startSound} autostart="false" />
         </div>
       </div>
-    )
+    );
   }
 
   toggleTimer = () => {
-    const time = formatTimeToString(this.state.time)
+    const time = formatTimeToString(this.state.time);
 
     this.setState({ active: !this.state.active }, () => {
       if (this.state.active) {
-        ipcRenderer.send('update-workt-status', 'Stop', time)
+        ipcRenderer.send('update-workt-status', 'Stop', time);
 
         if (this.state.time === WORK_TIME) {
-          document.getElementById('audio-start').play()
+          document.getElementById('audio-start').play();
         }
-        this.timer = setInterval(this.tick, 1000)
+        this.timer = setInterval(this.tick, 1000);
       } else {
-        ipcRenderer.send('update-workt-status', 'Start', time)
-        clearInterval(this.timer)
+        ipcRenderer.send('update-workt-status', 'Start', time);
+        clearInterval(this.timer);
       }
-    })
-  }
+    });
+  };
 
   skipBreak = () => {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
 
     this.setState({
       active: false,
       time: WORK_TIME,
       stage: 'work'
-    })
-  }
+    });
+  };
 
   tick = () => {
     if (this.state.time === 0) {
-      const isPomodoroEnd = this.state.stage === 'work'
+      const isPomodoroEnd = this.state.stage === 'work';
 
       if (isPomodoroEnd) {
-        document.getElementById('audio-end').play()
+        document.getElementById('audio-end').play();
       }
 
-      const stage = isPomodoroEnd ? 'relax' : 'work'
-      const active = isPomodoroEnd
-      const total = isPomodoroEnd ? this.state.total + 1 : this.state.total
-      const time = isPomodoroEnd ? RELAX_TIME : WORK_TIME
-      const series = getNewSeries(this.state.stage, this.state.series)
+      const stage = isPomodoroEnd ? 'relax' : 'work';
+      const active = isPomodoroEnd;
+      const total = isPomodoroEnd ? this.state.total + 1 : this.state.total;
+      const time = isPomodoroEnd ? RELAX_TIME : WORK_TIME;
+      const series = getNewSeries(this.state.stage, this.state.series);
 
-      this.setState({ stage, total, time, active, series })
+      this.setState({ stage, total, time, active, series });
 
       if (!active) {
-        clearInterval(this.timer)
+        clearInterval(this.timer);
       }
     } else {
-      const newTime = this.state.time - 1
+      const newTime = this.state.time - 1;
       this.setState({ time: newTime }, () => {
-        ipcRenderer.send('update-tray-title', formatTimeToString(newTime))
-      })
+        ipcRenderer.send('update-tray-title', formatTimeToString(newTime));
+      });
     }
-  }
+  };
 
   resetTimer = () => {
-    this.setState(initialState)
-  }
+    this.setState(initialState, () => {
+      clearInterval(this.timer);
+      ipcRenderer.send('update-tray-title', '25:00');
+    });
+  };
 }
