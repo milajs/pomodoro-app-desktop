1ac8e0900ab575d3996b0628a8f0bfda4203b3f8
mila.pro <lpromyslova@mail.ru>
2018-07-17 21:06:24 +0300

    constants

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,4 +1,4 @@
-export const WORK_TIME = 0.5 * 60;
-export const RELAX_TIME = 0.5 * 60;
+export const WORK_TIME = 25 * 60
+export const RELAX_TIME = 5 * 60
 
-export const WORK_TIME_TEXT = '25:00';
+export const WORK_TIME_TEXT = '25:00'
