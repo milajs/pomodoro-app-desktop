f23e9ca540721177d03988fb743d0ad67d3ed031
mila.pro <lpromyslova@mail.ru>
2018-09-27 22:57:49 +0300

    clean up timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,82 +1,62 @@
 import React from 'react'
-import { CSSTransition } from 'react-transition-group'
 
 import { formatTimeToString } from '../../utils/timer'
 
 import { WORK_TIME, RELAX_TIME } from '../../constants'
-
-const playIcon = require('../../assets/play.png')
-const pauseIcon = require('../../assets/pause.png')
 
 const VIEW_SIDE = 260
 const CENTER = VIEW_SIDE / 2
 const RADIUS = 121
 const CIRCLE_LENGHT = 2 * Math.PI * RADIUS
 
-export default function Timer({ active, time, stage, toggleTimer, skipBreak }) {
-  const src = active ? pauseIcon : playIcon
+export default function Timer({ time, stage }) {
   const formattedTime = formatTimeToString(time)
 
   const fullTime = stage === 'work' ? WORK_TIME : RELAX_TIME
   const progress = time / fullTime
 
-  return [
-    <svg
-      height={VIEW_SIDE}
-      width={VIEW_SIDE}
-      className="rotate90"
-      key="timer-wrapper"
-    >
-      <circle
-        r={RADIUS}
-        cx={CENTER}
-        cy={CENTER}
-        fill="none"
-        strokeWidth={15}
-        strokeDasharray={[5, 5]}
-        className="strokeMain"
-        stroke="#E53935"
-      />
-      <circle
-        r={RADIUS}
-        cx={CENTER}
-        cy={CENTER}
-        fill="none"
-        strokeWidth={15}
-        className="strokeBg"
-        strokeDasharray={CIRCLE_LENGHT}
-        strokeDashoffset={-CIRCLE_LENGHT * (1 - progress)}
-      />
-      <circle
-        r={RADIUS}
-        cx={CENTER}
-        cy={CENTER}
-        fill="none"
-        strokeWidth={15}
-        strokeDasharray={[5, 5]}
-        className="strokeLight"
-      />
-    </svg>,
-
-    <div className="timer" key="timer">
-      <p>{formattedTime}</p>
-    </div>,
-
-    <div key="buttons" className="btnsRow">
-      <button className="startButton" onClick={toggleTimer}>
-        <img src={src} alt="play" className={active ? '' : "playOffset"} />
-      </button>
-
-      <CSSTransition
-        timeout={300}
-        unmountOnExit
-        classNames="slideIn"
-        in={stage === 'relax'}
+  return (
+    <div className="timerContainer">
+      <svg
+        height={VIEW_SIDE}
+        width={VIEW_SIDE}
+        className="rotate90"
+        key="timer-wrapper"
       >
-        <button className="startButton" onClick={skipBreak}>
-          Skip break
-        </button>
-      </CSSTransition>
+        <circle
+          r={RADIUS}
+          cx={CENTER}
+          cy={CENTER}
+          fill="none"
+          strokeWidth={15}
+          strokeDasharray={[5, 5]}
+          className="strokeMain"
+          stroke="#E53935"
+        />
+        <circle
+          r={RADIUS}
+          cx={CENTER}
+          cy={CENTER}
+          fill="none"
+          strokeWidth={15}
+          className="strokeBg"
+          strokeDasharray={CIRCLE_LENGHT}
+          strokeDashoffset={-CIRCLE_LENGHT * (1 - progress)}
+        />
+        <circle
+          r={RADIUS}
+          cx={CENTER}
+          cy={CENTER}
+          fill="none"
+          strokeWidth={15}
+          strokeDasharray={[5, 5]}
+          className="strokeLight"
+        />
+      </svg>
+
+      <div className="timer" key="timer">
+        <p>{formattedTime}</p>
+      </div>
     </div>
-  ]
+  )
 }
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,23 +1,41 @@
 import React, { PureComponent } from 'react'
+import { CSSTransition } from 'react-transition-group'
 
 import Timer from './Timer'
 import Stats from './Stats'
 
-export default class MainContainer extends PureComponent {
+const playIcon = require('../../assets/play.png')
+const pauseIcon = require('../../assets/pause.png')
+
+export default class TimernContainer extends PureComponent {
   render() {
+    const src = this.props.active ? pauseIcon : playIcon
+
     return (
       <div className="container">
-        <div className="timer-container">
-          <Timer
-            time={this.props.time}
-            stage={this.props.stage}
-            active={this.props.active}
-            skipBreak={this.props.skipBreak}
-            toggleTimer={this.props.toggleTimer}
-          />
+        <Timer
+          time={this.props.time}
+          stage={this.props.stage}
+        />
 
-          <Stats total={this.props.total} series={this.props.series} />
+        <div key="buttons" className="btnsRow">
+          <button className="startButton" onClick={this.props.toggleTimer}>
+            <img src={src} alt="play" className={this.props.active ? '' : "playOffset"} />
+          </button>
+
+          <CSSTransition
+            timeout={300}
+            unmountOnExit
+            classNames="slideIn"
+            in={this.props.stage === 'relax'}
+          >
+            <button className="startButton" onClick={this.props.skipBreak}>
+              Skip break
+            </button>
+          </CSSTransition>
         </div>
+
+        <Stats total={this.props.total} series={this.props.series} />
       </div>
     )
   }
